0b9b21b5f5d4e0c0ec59fe7db55822016f48725c
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-05-31 12:25:39 +0000

    sd

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,6 +12,6 @@ if last_int > 5:
 elif last_int == 0:
     string = "and is 0"
 elif last_int < 6 and last_int != 0:
-    sting = "and is less than 6 and not 0"
+    string = "and is less than 6 and not 0"
 
 print("Last digit of {:d} is {:d}".format(number, last_int), string)
