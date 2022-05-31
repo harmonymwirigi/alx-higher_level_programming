bd9f896fbfed6b30ed4df85af6d9a106722e9199
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-05-31 12:14:53 +0000

    df

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,10 +9,10 @@ else:
     last_int = int(last)
 if last_int > 5:
     print("Last digit of {:d} is {:d} and is greater than 5".
-    format(number, last_int))
+format(number, last_int))
 elif last_int == 0:
     print("Last digit of {:d} is {:d} and is 0".
-    format(number, last_int))
+format(number, last_int))
 elif last_int < 6 and last_int != 0:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0".
-    format(number, last_int))
+format(number, last_int))
