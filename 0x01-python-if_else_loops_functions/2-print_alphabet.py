ad373976a2af027e270aceb0ebecd7a9b2fd6997
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-05-31 12:50:06 +0000

    g

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-alpha = 26
-res = ""
-for i in range(97, 97 + alpha):
-    res = res + chr(i)
-print("{}".format(res))
+for i in range(97, 123):
+    print("{:c}".format(i), end='')
