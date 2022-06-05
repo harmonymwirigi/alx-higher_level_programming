01165d32cc843be1b8f50998d6efb5783d02661e
harmonymwirigi <harmonymwirigi99.com>
2022-06-05 07:50:59 -0700

    reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,4 +7,4 @@ def print_reversed_list_integer(my_list=[]):
     if type(my_list) is list:
         my_list.reverse()
         for j in my_list:
-            print("{:d}",format(j))
+            print("",format(j))
