24a2c7393dfcadd434bb02953e6fb7e3316c9327
harmonymwirigi <harmonymwirigi99.com>
2022-06-05 07:20:16 -0700

    replacing

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,8 +5,9 @@ def replace_in_list(my_list, idx, element):
     replacing element at index with element
     """
     if idx < 0:
-        return
-    elif idx > len(my_list):
+        return my_list
+    elif idx >= len(my_list):
         return my_list
     else:
         my_list[idx] = element
+        return my_list
