8ca55c10544d0176f168aa4734b6cfa296590cc3
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-06-10 11:06:05 +0000

    simple delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,4 +3,6 @@ def simple_delete(a_dictionary, key=""):
     """
     function that deletes a key in a dictionary.
     """
-    return (del a_dictionary[key])
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return (a_dictionary.copy())
