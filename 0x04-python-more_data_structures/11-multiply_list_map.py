aae341c7eb86fbd9f6438a21ffee4943fcce7e20
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-06-10 11:33:52 +0000

    roman integer

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -3,4 +3,4 @@ def multiply_list_map(my_list=[], number=0):
     """
      a list with all values multiplied by a number without using any loops
     """
-    return [x*number for x in my_list]
+    return (list(map(lambda x: x*number, my_list)))
