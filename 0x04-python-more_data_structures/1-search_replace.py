f413b9ba60ea81b2af17e271d9050cb2de594a32
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-06-10 10:02:42 +0000

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     """
-    serches for an element search in list and replaces it
+    serches for an element search in list and replaces i
     """
-    for i in my_list:
-        if i = search:
-            d = my_list.index(i)
-            my_list[d] = replace
-    return (my_list)
+    return ([elem if elem != search else replace for elem in my_list])
