10d521f28b5b824a19f8c9d780be085e0b45b8fa
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-06-10 09:37:51 +0000

    message

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """
+    compute the square of all integers in a matrix
+    """
     for i in matrix:
         for j in i:
             k = j**2
