f9a554a20eec238af2c6e5f265da36897f849cf8
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-06-10 11:23:20 +0000

    mulitply list

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+    """
+     a list with all values multiplied by a number without using any loops
+    """
+    return [x*number for x in my_list]
