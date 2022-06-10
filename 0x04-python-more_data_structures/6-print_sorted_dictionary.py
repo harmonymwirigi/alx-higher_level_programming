2ab7f17e824e4cb5b4a7be2ce74a1987919a6e57
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-06-10 10:41:21 +0000

    sorted keys

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    """
+    oder the keys of a dictionary
+    """
+    [print ("{} : {}".format(key, a_dictionary[key]))
+        for key in sorted(a_dictionary)]
+
