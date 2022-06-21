1cf898385a923da1938c71fa72b06f2af667fc43
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-06-21 19:55:36 +0000

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 14:21:54 2020
+@author: Robinson Montes
+"""
 class Square:
+    """Empty class Square that defines a square
+    Attributes:
+        empty
+    """
     pass
