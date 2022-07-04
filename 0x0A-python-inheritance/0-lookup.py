45013db35bd30a988160c30ecf96d98438f1c3ad
harmonymwirigi <harmonymwirigi99.com>
2022-07-04 08:25:08 -0700

    my first commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+#0-lookup.py
+
+"""
+print the instances and function in an object
+"""
 
 def lookup(obj):
+    """
+    function to return the list of objects ad functions
+    Args:
+        obj: the object of the class
+    """
     k = dir(obj)
     return k
