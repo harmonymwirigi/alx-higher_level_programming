733cf609ce798f5c7e714422ca5b3dd4acbc25da
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-07-06 10:21:19 +0000

    insert into file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,8 +8,10 @@ Return:
 	the number of characters inserted
 """
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding = "utf-8") as f:
+    """ creates the file if it doeas not exist """
+    with open(filename, "w+", encoding = "utf-8") as f:
         f.write(text)
+    """" read the file and returns the length"""
     with open(filename, "r", encoding = "utf-8") as f:
         line = f.read()
     return len(line)
