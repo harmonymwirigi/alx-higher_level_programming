51325bbff1e0c7b62338c681b371933e47d0eec5
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-07-06 09:41:30 +0000

    mistakes rectified

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-
+"""
+function read_file to read the file file name
+argr:
+	filename : the file to be read
+"""
 def read_file(filename=""):
+	""" with to ensure the file is closed after use"""
 	with open(filename, "r", encoding = "utf-8") as f:
 		f.read()
